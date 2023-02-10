7b80765143a3296ae74272613cee89054f047477
Kirill Shnyrev <kirill.shnyrev@multilogin.com>
2023-02-11 00:40:49 +0100

    Add lesson_19 Task5 solution

--- basic_programming/lesson_19/code/practice/src/Task5.java
+++ basic_programming/lesson_19/code/practice/src/Task5.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Task5 {
   // Пирожок в столовой стоит a рублей и b копеек.
   // Определите, сколько рублей и копеек нужно заплатить за n пирожков.
@@ -11,4 +15,15 @@ public class Task5 {
   // 4
   // Пример вывода:
   // 10 0
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int a = Integer.parseInt(br.readLine());
+    int b = Integer.parseInt(br.readLine());
+    int n = Integer.parseInt(br.readLine());
+
+    int priceK = a * 100 + b;
+    int total = priceK * n;
+    System.out.println(total / 100 + " " + total % 100);
+  }
 }
