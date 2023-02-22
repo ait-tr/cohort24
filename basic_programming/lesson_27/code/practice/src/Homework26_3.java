56c762cc5c0312fd2bda0e73ba251953c8c8b994
Kirill Shnyrev <kirill.shnyrev@multilogin.com>
2023-02-22 21:22:57 +0100

    Add lesson_27

---
+++ basic_programming/lesson_27/code/practice/src/Homework26_3.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Homework26_3 {
+
+  // Вам дан словарь, состоящий из пар слов. Каждое слово является синонимом к парному ему слову.
+  // Все слова в словаре различны.
+  //
+  // Для слова из словаря, записанного в последней строке, определите его синоним.
+  //
+  // ## Формат ввода
+  // - в первой строке - количество пар в словаре
+  // - в следующих строках - сами записи: первое слово (ключ) и второе слово (синоним), каждое
+  // слово в отдельной строке
+  // - в последней строке - слово, синоним для которого нужно вывести
+  //
+  // ## Пример ввода
+  // 3
+  // Hello
+  // Hi
+  // Bye
+  // Goodbye
+  // List
+  // Array
+  // Goodbye
+  //
+  // ## Пример вывода
+  // Bye
+
+  public static void main(String[] args) throws IOException {
+    try {
+      // словарь.keySet() - множество ключей
+      // словарь.values() - множество значений
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      Map<String, String> synonyms = new HashMap<>();
+      int n = Integer.parseInt(br.readLine());
+      for (int i = 0; i < n; ++i) {
+        String first = br.readLine();
+        String second = br.readLine();
+        synonyms.put(first, second); // Bye -> Goodbye
+        synonyms.put(second, first); // Goodbye -> Bye
+      }
+      String word = br.readLine();
+      if (synonyms.containsKey(word)) {
+        String synonym = synonyms.get(word);
+        System.out.println(synonym);
+      } else {
+        System.out.println("В словаре нет такого слова");
+      }
+    } catch (NumberFormatException e) {
+      System.err.println("Неправильный формат числа: " + e.getMessage());
+    }
+  }
+}
