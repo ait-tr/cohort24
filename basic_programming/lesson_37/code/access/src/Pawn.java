a49a52a9ea629302bb0acb7bb70bc585a87183db
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-09 21:02:17 +0100

    Refactor Pawn

--- basic_programming/lesson_37/code/access/src/Pawn.java
+++ basic_programming/lesson_37/code/access/src/Pawn.java
@@ -12,23 +12,8 @@ public class Pawn {
   public Pawn(Color color, int row, int column) {
     this.color = color;
 
-    if (row < 1 || row > 8) {
-      throw new IllegalArgumentException("Некорректный номер строки: " + row);
-      // ошибка - неправильный аргумент
-      // выбрасываем её самостоятельно - теперь наш собственный код может сообщать об ошибке
-      // throw (объект класса Исключение);
-      // throw new КлассИсключения();
-      // КлассИсключения() - конструктор по умолчанию, "просто ошибка"
-      // КлассИсключения(String сообщениеОбОшибке) - ошибка "с подробностями",
-      //  их видно через getMessage()
-      // условие-стражник для метода, в котором нельзя просто написать return
-    }
+    checkCoordinates(row, column);
     this.row = row;
-
-    if (column < 1 || column > 8) {
-      System.out.println("Некорректный номер столбца: " + column);
-      throw new IllegalArgumentException();
-    }
     this.column = column;
   }
 
@@ -49,6 +34,29 @@ public class Pawn {
   // public, потому что сеттер
   // не static, потому что для конкретной фигуры
   public void setCoordinates(int row, int column) {
+    checkCoordinates(row, column);
+    // TODO проверка возможности хода
+  }
 
+  // проверка корректности координат
+  // private - потому что "служебный"
+  // static - не связан с конкретной фигурой, предварительная проверка
+  private static void checkCoordinates(int row, int column) {
+    if (row < 1 || row > 8) {
+      throw new IllegalArgumentException("Некорректный номер строки: " + row);
+      // ошибка - неправильный аргумент
+      // выбрасываем её самостоятельно - теперь наш собственный код может сообщать об ошибке
+      // throw (объект класса Исключение);
+      // throw new КлассИсключения();
+      // КлассИсключения() - конструктор по умолчанию, "просто ошибка"
+      // КлассИсключения(String сообщениеОбОшибке) - ошибка "с подробностями",
+      //  их видно через getMessage()
+      // условие-стражник для метода, в котором нельзя просто написать return
+    }
+
+    if (column < 1 || column > 8) {
+      System.out.println("Некорректный номер столбца: " + column);
+      throw new IllegalArgumentException();
+    }
   }
 }
