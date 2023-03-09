dbb82a9eca544498e2b3c2a9dfccb6069cb58d02
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-09 21:08:34 +0100

    Add static method usage to HomeworkMain

--- basic_programming/lesson_37/code/access/src/homework_36/HomeworkMain.java
+++ basic_programming/lesson_37/code/access/src/homework_36/HomeworkMain.java
@@ -1,6 +1,6 @@
 package homework_36;
 
-public class Main {
+public class HomeworkMain {
 
   public static void main(String[] args) {
     Soda firstGlassOfSoda = new Soda(null);
@@ -13,5 +13,11 @@ public class Main {
     Triangle t1 = new Triangle(-1, 2, 3); // отрицательная сторона
     Triangle t2 = new Triangle(1, 2, 3); // не выполняется неравенство треугольника
     Triangle t3 = new Triangle(3, 4, 5); // всё хорошо
+
+    int a = 2;
+    int b = 4;
+    int c = 6;
+    // используем статический метод
+    System.out.println(Triangle.checkSides(a, b, c) ? "YES" : "NO");
   }
 }
