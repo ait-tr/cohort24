e96076dd752644df6cbf329ec17c03de5376ba15
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-09 18:51:12 +0100

    Add Triangle usage to homework_36.Main

--- basic_programming/lesson_37/code/access/src/homework_36/Main.java
+++ basic_programming/lesson_37/code/access/src/homework_36/Main.java
@@ -9,5 +9,9 @@ public class Main {
     System.out.println(secondGlassOfSoda.getMyDrinkString());
     Soda thirdGlassOfSoda = new Soda("малина");
     System.out.println(thirdGlassOfSoda.getMyDrinkString());
+
+    Triangle t1 = new Triangle(-1, 2, 3); // отрицательная сторона
+    Triangle t2 = new Triangle(1, 2, 3); // не выполняется неравенство треугольника
+    Triangle t3 = new Triangle(3, 4, 5); // всё хорошо
   }
 }
