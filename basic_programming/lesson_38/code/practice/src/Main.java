39b1983050522df3bdac1d3766682c572d3a19c0
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-10 19:57:51 +0100

    Add Main.java

---
+++ basic_programming/lesson_38/code/practice/src/Main.java
@@ -0,0 +1,18 @@
+import chess.Color;
+import chess.Pawn;
+import students.Student;
+
+public class Main {
+
+  // написать базу данных студентов
+  // студенты могут находиться в группах
+
+  // прочитать количество групп
+  // для каждой группы:
+  // - прочитать количество студентов
+  // - прочитать информацию о студентах - "имя" или "имя,e-mail" для каждого в отдельной строке
+  // красиво вывести состав групп на экран
+  public static void main(String[] args) {
+    Student student = new Student("Kirill", "");
+  }
+}
