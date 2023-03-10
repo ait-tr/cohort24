52e7f9e71071350410e2caac46474bcea2d6d236
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-10 21:00:40 +0100

    Finish Students task

--- basic_programming/lesson_38/code/practice/src/Main.java
+++ basic_programming/lesson_38/code/practice/src/Main.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import students.Student;
 
 public class Main {
@@ -5,16 +10,32 @@ public class Main {
   // написать базу данных студентов
   // студенты могут находиться в группах
 
-  // прочитать количество групп
-  // для каждой группы:
+  // красиво вывести состав групп на экран
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    List<Student> students = new ArrayList<>();
+    // прочитать количество групп
+    int groups = Integer.parseInt(br.readLine());
+    for (int groupId = 0; groupId < groups; ++groupId) {
+      // для каждой группы:
+      readGroup(br, students);
+    }
+    for (Student student : students) {
+      System.out.printf("%s (%s) в группе %s%n", student.getName(), student.getEMail(),
+          student.getGroup());
+    }
+  }
+
   // - прочитать название группы
   // - прочитать количество студентов
   // - прочитать информацию о студентах - "имя" или "имя,e-mail" для каждого в отдельной строке
-  // красиво вывести состав групп на экран
-  public static void main(String[] args) {
-    Student student = Student.parseStudent("cohort24", "Vadim,Vadim@cohort24.info");
-    Student student2 = Student.parseStudent("cohort24", "Alexey,Alexey@cohort24.info");
-    Student student3 = Student.parseStudent("cohort24", "Artem,Artem@cohort24.info");
-    System.out.println();
+  private static void readGroup(BufferedReader br, List<Student> students) throws IOException {
+    String groupName = br.readLine();
+    int studentsNumber = Integer.parseInt(br.readLine());
+    for (int i = 0; i < studentsNumber; ++i) {
+      String line = br.readLine();
+      Student student = Student.parseStudent(groupName, line);
+      students.add(student);
+    }
   }
 }
