f0caa0c2e79e9bea338741d5b0c4975278051d3a
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-11 10:58:14 +0100

    Correct Main to use updated Money

--- basic_programming/consultation_03/code/practice/src/Main.java
+++ basic_programming/consultation_03/code/practice/src/Main.java
@@ -21,7 +21,8 @@ public class Main {
   // - либо сразу пишем методы
   // - либо сначала пишем программу в целом, используя только заготовки методов
   public static void main(String[] args) {
-    Item tovar1 = new Item("art1", "Молоко", new Money());
+    Item tovar1 = new Item("art1", "Молоко", new Money(75, "RUB"));
     System.out.println(tovar1.getTitle());
+    System.out.println(tovar1.getPrice().getMoneyString());
   }
 }
