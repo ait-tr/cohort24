33b651ef52897e9f2cc77e2587729cb0a7fd171b
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-11 10:27:05 +0100

    Define class members

--- basic_programming/consultation_03/code/practice/src/Item.java
+++ basic_programming/consultation_03/code/practice/src/Item.java
@@ -1,3 +1,10 @@
 public class Item {
 
+  // Для каждого товара необходимо знать следующие вещи:
+  // артикул - уникальная строка
+  final private String article;
+  // название - строка
+  private String title;
+  // стоимость - специальный класс Деньги
+  private Money price;
 }
--- basic_programming/consultation_03/code/practice/src/Money.java
+++ basic_programming/consultation_03/code/practice/src/Money.java
@@ -1,3 +1,5 @@
 public class Money {
 
+  private long amount; // в "копейках" - минимальных целых единицах
+  private String currency; // валюта
 }
