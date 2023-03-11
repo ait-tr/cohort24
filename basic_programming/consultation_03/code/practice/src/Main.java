069b824dd5e0ecb64299f6fff388628db4c5af0d
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-11 11:22:48 +0100

    Add itemsBase filling

--- basic_programming/consultation_03/code/practice/src/Main.java
+++ basic_programming/consultation_03/code/practice/src/Main.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
   // - файл Main
@@ -20,8 +26,35 @@ public class Main {
   // - Определяем схему использования этих методов в Main
   // - либо сразу пишем методы
   // - либо сначала пишем программу в целом, используя только заготовки методов
-  public static void main(String[] args) {
-    Item tovar1 = new Item("art1", "Молоко", new Money(75, "RUB"));
-    System.out.println(tovar1.getItemString());
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    Map<String, Item> itemsBase = new HashMap<>(); // база "артикул - товар"
+
+    System.out.print("Введите количество товаров: ");
+    int itemsAmount = Integer.parseInt(br.readLine());
+    for (int i = 0; i < itemsAmount; ++i) {
+      System.out.print("Введите артикул товара: ");
+      String article = br.readLine();
+      // проверяем на уникальность
+      while (itemsBase.containsKey(article)) {
+        System.out.println("Артикул не уникален!");
+        System.out.print("Введите артикул товара: ");
+        article = br.readLine();
+      }
+
+      System.out.print("Введите название товара: ");
+      String title = br.readLine();
+
+      System.out.print("Введите стоимость (дробное число):");
+      Double price = Double.parseDouble(br.readLine());
+
+      System.out.print("Введите валюту: ");
+      String currency = br.readLine();
+
+      Money itemPrice = new Money(price, currency);
+      Item item = new Item(article, title, itemPrice);
+      itemsBase.put(article, item);
+    }
   }
 }
