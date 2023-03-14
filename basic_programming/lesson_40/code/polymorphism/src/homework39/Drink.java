bd1bc519b5e2cf8b6583db44b8f9bb7cae736b41
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-14 18:13:13 +0100

    Add Age check

--- basic_programming/lesson_40/code/polymorphism/src/homework39/Drink.java
+++ basic_programming/lesson_40/code/polymorphism/src/homework39/Drink.java
@@ -1,5 +1,9 @@
 package homework39;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 // Создать класс `Drink` (напиток) - наследника класса `Food`. В классе должны быть дополнительно:
 // - метод "открыть", который:
 //   - для газированного напитка печатает "пшш",
@@ -7,6 +11,7 @@ package homework39;
 //   - а для алкогольного сначала спрашивает возраст и издаёт звук, только если возраст больше 18.
 public class Drink extends Food {
 
+  final private static int AGE_LIMIT = 18;
   private boolean sparkling;
   private boolean alcohol;
 
@@ -14,5 +19,20 @@ public class Drink extends Food {
     super(title, kcal); // super() - конструктор базового класса
     this.sparkling = sparkling;
     this.alcohol = alcohol;
+  }
+
+  public void open() throws IOException {
+    if (alcohol) {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      System.out.print("Введите возраст: ");
+      int age = Integer.parseInt(br.readLine());
+      if (age < AGE_LIMIT) { // условие-стражник
+        System.out.println("К сожалению, вам нельзя алкоголь");
+        return;
+      }
+    }
+
+
   }
 }
