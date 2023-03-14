576c69373fb571421838b61e4b629f48f7535995
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-14 18:07:06 +0100

    Add Drink constructor

--- basic_programming/lesson_40/code/polymorphism/src/homework39/Drink.java
+++ basic_programming/lesson_40/code/polymorphism/src/homework39/Drink.java
@@ -1,15 +1,18 @@
 package homework39;
 
 // Создать класс `Drink` (напиток) - наследника класса `Food`. В классе должны быть дополнительно:
-// - поле "газированный" (да/нет)
-// - поле "алкогольный" (да/нет)
 // - метод "открыть", который:
 //   - для газированного напитка печатает "пшш",
 //   - для негазированного - "скр",
 //   - а для алкогольного сначала спрашивает возраст и издаёт звук, только если возраст больше 18.
 public class Drink extends Food {
 
-  public Drink(String title, double kcal) {
-    super(title, kcal);
+  private boolean sparkling;
+  private boolean alcohol;
+
+  public Drink(String title, double kcal, boolean sparkling, boolean alcohol) {
+    super(title, kcal); // super() - конструктор базового класса
+    this.sparkling = sparkling;
+    this.alcohol = alcohol;
   }
 }
