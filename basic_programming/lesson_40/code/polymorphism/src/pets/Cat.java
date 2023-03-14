0f2f7460827b5643c11a65fbf2584ee52eff5bf8
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-14 19:08:49 +0100

    Override method makeSomeNoise()

--- basic_programming/lesson_40/code/polymorphism/src/pets/Cat.java
+++ basic_programming/lesson_40/code/polymorphism/src/pets/Cat.java
@@ -9,4 +9,9 @@ public class Cat extends pets.Pet {
   public void makePurr() {
     System.out.println(getName() + ": Мррр...");
   }
+
+  @Override // Перезаписать
+  public void makeSomeNoise() {
+    System.out.println(getName() + ": Мяу!");
+  }
 }
--- basic_programming/lesson_40/code/polymorphism/src/pets/Dog.java
+++ basic_programming/lesson_40/code/polymorphism/src/pets/Dog.java
@@ -10,4 +10,10 @@ public class Dog extends Pet {
     weight -= 0.001;
     System.out.println(getName() + ": Собаки могут носить тапочки. А могут и не носить...");
   }
+
+  @Override // Перезаписать
+  public void makeSomeNoise() {
+//    super.makeSomeNoise(); // так можно вызвать метод базового класса
+    System.out.println(getName() + ": Гав!");
+  }
 }
