951f39920602d9f7baa7e4140f2db0bb23a71a34
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-14 19:45:16 +0100

    Add Phone

---
+++ basic_programming/lesson_40/code/polymorphism/src/PhonesMain.java
@@ -0,0 +1,9 @@
+import phones.Phone;
+
+public class PhonesMain {
+
+  public static void main(String[] args) {
+    Phone prostoTelephon = new Phone();
+    prostoTelephon.call("+1234567");
+  }
+}
---
+++ basic_programming/lesson_40/code/polymorphism/src/phones/Phone.java
@@ -0,0 +1,8 @@
+package phones;
+
+public class Phone {
+
+  public void call(String phoneNumber) {
+    System.out.printf("Мы звоним по телефону %s%n", phoneNumber);
+  }
+}
