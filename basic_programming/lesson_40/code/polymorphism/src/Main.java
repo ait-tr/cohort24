048e853a73123e9fe3581a3eda8f5e9d5691e621
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-14 18:45:04 +0100

    Add polymorphism theory

--- basic_programming/lesson_40/code/polymorphism/src/Main.java
+++ basic_programming/lesson_40/code/polymorphism/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
 
+  // Полиморфизм (polymorphism): Один интерфейс — много реализаций
+  // Например: замена ArrayList на LinkedList
+  // у них совпадают интерфейсы:
+  // - метод add()
+  // - метод get()
+  // - метод size()
+  // - возможность перебирать через for-each
+
+  // другой пример - замена InputStreamReader на FileReader
   public static void main(String[] args) {
     System.out.println("Hello world!");
   }
