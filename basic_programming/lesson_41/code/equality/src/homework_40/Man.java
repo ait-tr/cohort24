b12b7218302b6860740f3b007e7706baf8ce11ea
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-15 17:59:08 +0100

    Add Man and Woman

---
+++ basic_programming/lesson_41/code/equality/src/homework_40/Man.java
@@ -0,0 +1,8 @@
+package homework_40;
+
+public class Man extends Person {
+
+  public Man(String name) {
+    super(name); // при создании мужчины создаётся человек
+  }
+}
---
+++ basic_programming/lesson_41/code/equality/src/homework_40/Woman.java
@@ -0,0 +1,8 @@
+package homework_40;
+
+public class Woman extends Person {
+
+  public Woman(String name) {
+    super(name); // при создании женщины создаётся человек
+  }
+}
