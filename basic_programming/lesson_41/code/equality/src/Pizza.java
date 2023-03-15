9d72f691b3316f7bf8aba7058739164ae28edc3d
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-15 21:04:50 +0100

    Add Pizza.equals to Pizza.hashCode

--- basic_programming/lesson_41/code/equality/src/Pizza.java
+++ basic_programming/lesson_41/code/equality/src/Pizza.java
@@ -12,4 +12,32 @@ public class Pizza {
   public String toString() {
     return "Пицца '" + title + "' весом " + weight + " г";
   }
+
+  // метод equals должен уметь сравнивать наш объект с ЛЮБЫМ
+  // возвращаем true, если объекты равны, и false, если не равны
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) { // мы сравниваем объект сам с собой (сравниваем ссылки)
+      return true;
+    }
+    // если obj является разновидностью класса Pizza, то
+    // превращаем его в объект otherPizza класса Pizza
+    if (!(obj instanceof Pizza otherPizza)) {
+      return false; // если "другой" объект - не пицца, то они не равны
+    }
+    // пиццы равны, только если совпадают названия и вес
+    return title.equals(otherPizza.title) && weight == otherPizza.weight;
+  }
+
+  // переопределение equals ДОЛЖНО приводить к переопределению hashCode()
+  // это специальный метод, по которому Hash* (HashSet, HashMap) решает, нужно ли вообще
+  // сравнивать наши объекты.
+  // Если hashCode() разный, то Hash* считает объекты РАЗНЫМИ и даже не пытается их сравнивать
+  // Если hashCode() одинаковый, то Hash* сравнивает объекты через equals()
+  @Override
+  public int hashCode() {
+    // !!! для одинаковых (через equals()) элементов hashCode() ОБЯЗАН возвращать одинаковый hash
+    // это значит, что мы должны учитывать все поля, которые сравниваются в equals() (и только их)
+    return 31 * weight * title.hashCode();
+  }
 }
--- basic_programming/lesson_41/code/equality/src/PizzaRunner.java
+++ basic_programming/lesson_41/code/equality/src/PizzaRunner.java
@@ -1,8 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class PizzaRunner {
 
@@ -11,7 +11,7 @@ public class PizzaRunner {
 
     System.out.print("Введите количество пицц: ");
     int n = Integer.parseInt(br.readLine());
-    List<Pizza> pizzas = new ArrayList<>(n);
+    Set<Pizza> pizzas = new HashSet<>();
     for (int i = 0; i < n; ++i) {
       System.out.println("Ввод данных для пиццы номер " + (i + 1) + ":");
       System.out.print("  Введите название: ");
@@ -20,10 +20,14 @@ public class PizzaRunner {
       int weight = Integer.parseInt(br.readLine());
 
       Pizza tempPizza = new Pizza(title, weight);
-      pizzas.add(tempPizza);
+      pizzas.add(tempPizza); // при проверке уникальности объекта Java сравнивает объекты через
+      // метод equals().
+      // Если "нашего" метода нет, то используется Object.equals().
+      // Object.equals() сравнивает ССЫЛКИ - если в памяти это два разных объекта (созданных через
+      // new), то и для Object.equals() это два разных объекта.
     }
 
-    System.out.println("У нас есть данные про " + pizzas.size() + " пицц:");
+    System.out.println("У нас есть данные про " + pizzas.size() + " уникальных пицц:");
     int k = 1;
     for (Pizza p : pizzas) {
       System.out.println(k + ". " + p);
