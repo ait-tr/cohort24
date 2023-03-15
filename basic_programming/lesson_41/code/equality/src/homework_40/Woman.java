1815b508071b203d6e84b2582b873329f88fa870
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-15 18:08:55 +0100

    Add Woman.toString()

--- basic_programming/lesson_41/code/equality/src/homework_40/Woman.java
+++ basic_programming/lesson_41/code/equality/src/homework_40/Woman.java
@@ -5,4 +5,9 @@ public class Woman extends Person {
   public Woman(String name) {
     super(name); // при создании женщины создаётся человек
   }
+
+  @Override
+  public String toString() {
+    return "Женщина по имени " + getName();
+  }
 }
