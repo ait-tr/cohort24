de1cdbb50094b176e6d1f8142a6cedb6306c1b7e
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-16 19:49:14 +0100

    Add reputation

--- basic_programming/lesson_42/code/abstracts/src/Account.java
+++ basic_programming/lesson_42/code/abstracts/src/Account.java
@@ -10,10 +10,12 @@ public class Account {
 
   private final String name;
   private final String email;
+  private int reputation;
 
   public Account(String name, String email) {
     this.name = name;
     this.email = email;
+    reputation = 0;
   }
 
   // сеттеров не будет (или будут приватными) - поля нельзя менять по условию
@@ -24,6 +26,34 @@ public class Account {
 
   public String getEmail() {
     return email;
+  }
+
+  /**
+   * Увеличивает репутацию.
+   *
+   * @param  diff
+   *         Разница между старым и новым значением, всегда положительная
+   */
+  public void increaseReputation(int diff) {
+    if (diff > 0) {
+      reputation += diff;
+    }
+  }
+
+  /**
+   * Уменьшает репутацию.
+   *
+   * @param  diff
+   *         Разница между старым и новым значением, всегда положительная
+   */
+  public void decreaseReputation(int diff) {
+    if (diff > 0) {
+      reputation -= diff;
+    }
+  }
+
+  public int getReputation() {
+    return reputation;
   }
 
   @Override
