7e41ad0edee15e83d4b983b93a01ff2f00041a46
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-17 18:03:43 +0100

    Add InvalidEmail catching

--- basic_programming/lesson_43/code/abstracts/src/homework_42/MainAccounts.java
+++ basic_programming/lesson_43/code/abstracts/src/homework_42/MainAccounts.java
@@ -45,7 +45,11 @@ public class MainAccounts {
     List<Account> result = new ArrayList<>(n);
     for (int k = 1; k <= n; ++k) {
       System.out.println("Введите данные учётной записи №" + k + ":");
-      result.add(readAccount(br));
+      try {
+        result.add(readAccount(br));
+      } catch (InvalidEmail e) {
+        System.out.println("Не получилось добавить учётную запись: " + e.getMessage());
+      }
     }
     return result;
   }
