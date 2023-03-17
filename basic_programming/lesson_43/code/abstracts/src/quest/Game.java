96fb3026b080a408000066acb2bebf612806b8bd
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-17 20:30:29 +0100

    Add Game and some methods

---
+++ basic_programming/lesson_43/code/abstracts/src/quest/Game.java
@@ -0,0 +1,22 @@
+package quest;
+
+import java.util.List;
+
+public class Game {
+
+  private Hero mainHero;
+  private List<Room> rooms;
+
+  public Game() {
+    // здесь заполняем комнаты
+  }
+
+  public void start() {
+    // создаём персонажа
+    mainCycle(); // запускаем основной игровой цикл
+  }
+
+  public void mainCycle() {
+    // основной игровой цикл
+  }
+}
