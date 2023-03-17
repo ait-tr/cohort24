57eb7d3fe6610019ede9db690da5e4c50929d2e9
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-17 18:25:56 +0100

    Add Human

---
+++ basic_programming/lesson_43/code/abstracts/src/Human.java
@@ -0,0 +1,16 @@
+public class Human {
+
+  private String name;
+
+  public Human(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
