ebefa4bc2537c7d3dd367da83a27d489abde3e40
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-17 20:35:25 +0100

    Add Game.start()

--- basic_programming/lesson_43/code/abstracts/src/Quest.java
+++ basic_programming/lesson_43/code/abstracts/src/Quest.java
@@ -8,13 +8,7 @@ public class Quest {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    System.out.println("=== Текстовый квест v 0.1.0 ===");
 
-    System.out.print("Введите имя: ");
-    String name = br.readLine();
-    Hero mainHero = new Hero(name); // mainHero - главный персонаж игры
-
-    Room mainRoom = new Room("Главный зал");
     // будет класс Game() - основной класс игры, который сможет:
     // - запускаться
     // - хранить в себе карту с комнатами
--- basic_programming/lesson_43/code/abstracts/src/quest/Game.java
+++ basic_programming/lesson_43/code/abstracts/src/quest/Game.java
@@ -1,18 +1,28 @@
 package quest;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.List;
 
 public class Game {
 
-  private Hero mainHero;
+  private Hero mainHero; // mainHero - главный персонаж игры
   private List<Room> rooms;
+  private Room current;
 
   public Game() {
-    // здесь заполняем комнаты
+    rooms.add(new Room("Зал"));
+    rooms.add(new Room("Кухня"));
+    rooms.add(new Room("Туалет"));
+    rooms.add(new Room("Коридор"));
   }
 
-  public void start() {
-    // создаём персонажа
+  public void start(BufferedReader br) throws IOException {
+    System.out.println("=== Текстовый квест v 0.1.0 ===");
+
+    System.out.print("Введите имя: ");
+    String name = br.readLine();
+    mainHero = new Hero(name);
     mainCycle(); // запускаем основной игровой цикл
   }
 
