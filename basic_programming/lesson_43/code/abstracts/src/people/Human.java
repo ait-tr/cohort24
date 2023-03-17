d5ca982d4ca93c7786b208870ba8e85eb8a38d0d
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-17 18:40:46 +0100

    Add retirement

--- basic_programming/lesson_43/code/abstracts/src/people/Human.java
+++ basic_programming/lesson_43/code/abstracts/src/people/Human.java
@@ -3,9 +3,11 @@ package people;
 public class Human {
 
   private String name;
+  private boolean retired;
 
   public Human(String name) {
     this.name = name;
+    this.retired = false;
   }
 
   public String getName() {
@@ -14,5 +16,16 @@ public class Human {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  // геттер
+  public boolean getRetired() {
+    return retired;
+  }
+
+  // сеттера нет, но есть метод
+  // выйти на пенсию
+  public void retire() {
+    this.retired = true;
   }
 }
