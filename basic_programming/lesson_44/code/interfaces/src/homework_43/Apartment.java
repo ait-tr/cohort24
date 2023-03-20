a4bb31d9db77d1204bc3edfb30ebd8cd25b685dc
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-20 17:44:32 +0100

    Add Mansion and Apartment

---
+++ basic_programming/lesson_44/code/interfaces/src/homework_43/Apartment.java
@@ -0,0 +1,9 @@
+package homework_43;
+
+// "Многоквартирный дом"
+public class Apartment extends House {
+
+  public Apartment(String address) {
+    super(address);
+  }
+}
---
+++ basic_programming/lesson_44/code/interfaces/src/homework_43/Mansion.java
@@ -0,0 +1,11 @@
+package homework_43;
+
+// Создайте классы "Частный дом"
+public class Mansion extends House {
+
+  public Mansion(String address) {
+    super(address);
+  }
+
+
+}
