6ed03eb07960fd9233d8773e08bc9af30cdfeb11
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-20 17:41:11 +0100

    Add address setter and getter

--- basic_programming/lesson_44/code/interfaces/src/homework_43/House.java
+++ basic_programming/lesson_44/code/interfaces/src/homework_43/House.java
@@ -1,10 +1,22 @@
 package homework_43;
 
+// Создайте абстрактный класс "Дом".
 public abstract class House {
 
   String address;
 
+  // В качестве аргумента конструктора принимайте адрес.
   public House(String address) {
     this.address = address;
+  }
+
+  // Напишите для адреса сеттер и геттер.
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getAddress() {
+    return address;
   }
 }
