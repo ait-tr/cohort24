55f0f9bcbee954fe7f7f47238e3b7102cefdff43
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-20 20:20:28 +0100

    Add Pager.address

--- basic_programming/lesson_44/code/interfaces/src/communications/Pager.java
+++ basic_programming/lesson_44/code/interfaces/src/communications/Pager.java
@@ -2,6 +2,24 @@ package communications;
 
 public class Pager implements Messenger {
 
+  String address; // адрес этого пейджера; если с него отправлять сообщения, то адрес отправителя
+
+  public Pager() {
+    address = "Без адреса";
+  }
+
+  public Pager(String address) {
+    this.address = address;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
   @Override
   public void sendMessage(String message, String address) {
     System.out.println("Пейджер: вы отправили сообщение '" + message + "' адресату " + address);
