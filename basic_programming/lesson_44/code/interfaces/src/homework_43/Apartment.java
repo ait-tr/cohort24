ccf0d4df92fb7fa8e91148b0ff0b07348bce483e
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-20 17:47:26 +0100

    Add Apartment.entrances

--- basic_programming/lesson_44/code/interfaces/src/homework_43/Apartment.java
+++ basic_programming/lesson_44/code/interfaces/src/homework_43/Apartment.java
@@ -3,7 +3,16 @@ package homework_43;
 // "Многоквартирный дом"
 public class Apartment extends House {
 
-  public Apartment(String address) {
+  private final int entrances;
+
+  // При создании многоквартирного дома дополнительно принимайте в конструкторе целое число -
+  // количество подъездов.
+  public Apartment(String address, int entrances) {
     super(address);
+    this.entrances = entrances;
+  }
+
+  public int getEntrances() {
+    return entrances;
   }
 }
