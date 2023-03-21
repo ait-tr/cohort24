e84a436ba3b19b16ff9922d9d4803e24070c93e0
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-21 21:01:35 +0100

    Add second variant

--- basic_programming/lesson_45/code/comparable/src/collections/Student.java
+++ basic_programming/lesson_45/code/comparable/src/collections/Student.java
@@ -33,30 +33,48 @@ public class Student implements Comparable<Student> {
   }
 
   // вариант 1 - сортировка по именам
+//  @Override
+//  public int compareTo(Student other) {
+//    // returns a negative integer, zero, or a positive integer as this object is less than,
+//    // equal to, or greater than the specified object.
+//
+//    // возвращает отрицательное целое число, ноль или положительное целое число,
+//    // поскольку этот объект меньше, равен или больше указанного объекта.
+//
+//    // если this < other - возвращаем любое отрицательное число (например, -1)
+//    // если this == other - возвращаем 0
+//    // если this > other - возвращаем любое положительное число (например, +1)
+//    // условие "x < y" равносильно условию "x.compareTo(y) < 0"
+////    if (/*this.*/name.compareTo(other.name) < 0) {
+////      // если имя this меньше (раньше по алфавиту), чем имя other, то и студент "меньше" -
+////      // должен быть раньше в списке
+////      return -1;
+////    }
+////    if (/*this.*/name.compareTo(other.name) > 0) {
+////      // если имя this больше (позже по алфавиту), чем имя other, то и студент "больше" -
+////      // должен быть позже в списке
+////      return 1;
+////    }
+////    // Если мы сюда попали, то имена совпадают. Можно вернуть 0
+////    return 0;
+//    return name.compareTo(other.name); // сравнить студентов - то же самое, что сравнить их имена
+//  }
+
+  // вариант 2 - сортировка по рейтингу
+  // если рейтинг одинаковый, пусть сортирует по алфавиту
+
   @Override
   public int compareTo(Student other) {
-    // returns a negative integer, zero, or a positive integer as this object is less than,
-    // equal to, or greater than the specified object.
-
-    // возвращает отрицательное целое число, ноль или положительное целое число,
-    // поскольку этот объект меньше, равен или больше указанного объекта.
-
-    // если this < other - возвращаем любое отрицательное число (например, -1)
-    // если this == other - возвращаем 0
-    // если this > other - возвращаем любое положительное число (например, +1)
-    // условие "x < y" равносильно условию "x.compareTo(y) < 0"
-//    if (/*this.*/name.compareTo(other.name) < 0) {
-//      // если имя this меньше (раньше по алфавиту), чем имя other, то и студент "меньше" -
-//      // должен быть раньше в списке
-//      return -1;
-//    }
-//    if (/*this.*/name.compareTo(other.name) > 0) {
-//      // если имя this больше (позже по алфавиту), чем имя other, то и студент "больше" -
-//      // должен быть позже в списке
-//      return 1;
-//    }
-//    // Если мы сюда попали, то имена совпадают. Можно вернуть 0
-//    return 0;
-    return name.compareTo(other.name); // сравнить студентов - то же самое, что сравнить их имена
+    // x < y  --> x.compareTo(y) < 0  --> x - y < 0
+    // x == y --> x.compareTo(y) == 0 --> x - y == 0
+    // x > y  --> x.compareTo(y) > 0  --> x - y > 0
+    // для чисел compareTo работает как "this - other"
+    // this < other
+    // this - other < 0
+    if (/*this.*/score != other.score) { // если рейтинг не одинаковый
+      return /*this.*/score - other.score; // то сравниваем рейтинг
+    }
+    // сюда попали, если рейтинг одинаковый, и сравниваем имена
+    return /*this.*/name.compareTo(other.name);
   }
 }
