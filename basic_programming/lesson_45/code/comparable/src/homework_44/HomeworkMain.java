4641bf940650d130365378466cb9d023bf998b71
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-21 18:09:45 +0100

    Add lists

--- basic_programming/lesson_45/code/comparable/src/homework_44/HomeworkMain.java
+++ basic_programming/lesson_45/code/comparable/src/homework_44/HomeworkMain.java
@@ -1,5 +1,8 @@
 package homework_44;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeworkMain {
 
   // # Задача 1
@@ -15,25 +18,39 @@ public class HomeworkMain {
   // в классах "Человек", "Утка", "Пингвин" и "Самолёт".
 
   public static void main(String[] args) {
+    List<CanFly> flyable = new ArrayList<>();
+    List<CanRun> runnable = new ArrayList<>();
+    List<CanSwim> swimmable = new ArrayList<>();
+
     Superman clarkKent = new Superman();
-    clarkKent.fly();
-    clarkKent.run();
-    clarkKent.swim("бассейне");
+    flyable.add(clarkKent);
+    runnable.add(clarkKent);
+    swimmable.add(clarkKent);
 
     Human kirill = new Human();
-    kirill.run();
-    kirill.swim("море");
+    // flyable.add(kirill); // ошибка - человек не умеет летать, Human не имеет отношения к CanFly
+    runnable.add(kirill);
+    swimmable.add(kirill);
 
     Duck donald = new Duck();
-    donald.fly();
-    donald.run();
-    donald.swim("озере");
+    flyable.add(donald);
+    runnable.add(donald);
+    swimmable.add(donald);
 
     Penguin skipper = new Penguin();
-    skipper.run();
-    skipper.swim("океане");
+    runnable.add(skipper);
+    swimmable.add(skipper);
 
     Plane plane = new Plane();
-    plane.fly();
+    flyable.add(plane);
+
+    for (CanFly flyingObject : flyable) {
+      flyingObject.fly();
+      // flyingObject.run(); // ошибка, мы ничего не знаем про run, у нас List<CanFly>
+      if (flyingObject instanceof Superman superman) {
+        System.out.println("ЭТО БЫЛ СУПЕРМЕН! Он умеет бегать");
+        superman.run();
+      }
+    }
   }
 }
