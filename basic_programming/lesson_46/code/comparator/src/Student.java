b3f94917b47d0b5a6f825472f1f3d13198a284ee
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-22 18:30:35 +0100

    Add comparison theory

--- basic_programming/lesson_46/code/comparator/src/Student.java
+++ basic_programming/lesson_46/code/comparator/src/Student.java
@@ -1,4 +1,4 @@
-public class Student {
+public class Student implements Comparable<Student> {
 
   private String name;
   private int score;
@@ -27,5 +27,38 @@ public class Student {
   @Override
   public String toString() {
     return name + ": " + score;
+  }
+
+  // по возрастанию рейтинга
+  // если this < other, то отрицательное
+  // если this = other, то 0
+  // если this > other, то положительное
+
+  // Немного математики:
+  // a < b  | +3
+  // a + 3 < b + 3  -- одинаковая операция +/- к каждой части неравенства сохраняет неравенство
+  // a - 4 < b - 4
+
+  // x < y  | -y
+  // x - y < y - y
+  // x - y < 0
+
+  // x > y  | -y
+  // x - y > 0
+
+  // x = y  | -y
+  // x - y = 0
+
+  // именно такое значение `x - y` (а точнее, `this - other`) и должно быть результатом
+  // метода `compareTo()`
+  @Override
+  public int compareTo(Student other) {
+    if (score < other.score) {
+      return -1;
+    }
+    if (score > other.score) {
+      return 1;
+    }
+    return 0;
   }
 }
