702bd6eefa3835a9928501c52c55cf39e2b7b37a
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-22 19:57:38 +0100

    Add Bill.readBill()

--- basic_programming/lesson_46/code/comparator/src/bills/Bill.java
+++ basic_programming/lesson_46/code/comparator/src/bills/Bill.java
@@ -1,5 +1,9 @@
 package bills;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Bill {
 
   private final String service; // услуга
@@ -21,5 +25,14 @@ public class Bill {
   @Override
   public String toString() {
     return String.format("Счёт за услугу \"%s\": %.2f", service, cost);
+  }
+
+  public static Bill readBill() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.print("Введите название услуги: ");
+    String service = br.readLine();
+    System.out.print("Введите стоимость услуги: ");
+    double cost = Double.parseDouble(br.readLine());
+    return new Bill(service, cost);
   }
 }
