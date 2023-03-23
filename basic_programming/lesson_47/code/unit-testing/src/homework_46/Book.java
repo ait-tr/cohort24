57e478d03172ee9cf8885beacfb7e8b3e2e306b3
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-23 17:37:27 +0100

    Add Book getters

--- basic_programming/lesson_47/code/unit-testing/src/homework_46/Book.java
+++ basic_programming/lesson_47/code/unit-testing/src/homework_46/Book.java
@@ -2,13 +2,25 @@ package homework_46;
 
 public class Book {
 
-  private String title;
-  private String author;
-  private int pages;
+  private final String title;
+  private final String author;
+  private final int pages;
 
   public Book(String title, String author, int pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public int getPages() {
+    return pages;
   }
 }
