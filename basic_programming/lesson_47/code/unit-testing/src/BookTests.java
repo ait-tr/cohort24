3c80116d9cd6510e7218780c5a76cebccb3120e6
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-23 21:00:06 +0100

    Test BookAuthorPagesComparator

--- basic_programming/lesson_47/code/unit-testing/src/BookTests.java
+++ basic_programming/lesson_47/code/unit-testing/src/BookTests.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class BookTests {
@@ -19,5 +21,29 @@ public class BookTests {
     }
     assertTrue(exceptionThrown);
 //    assertThrowsExactly(IllegalArgumentException.class, () -> new Book("", "", -5));
+  }
+
+  @Test
+  public void BookAuthorPagesComparator() {
+    // arrange - упорядочить, установить
+    Book book1 = new Book("Book", "A", 5);
+    Book book2 = new Book("Book", "B", 5);
+    Book book3 = new Book("Book", "B", 25);
+
+    List<Book> actual = new ArrayList<>();
+    actual.add(book2);
+    actual.add(book1);
+    actual.add(book3);
+
+    List<Book> expected = new ArrayList<>();
+    expected.add(book1);
+    expected.add(book3);
+    expected.add(book2);
+
+    // act - выполнить действие
+    actual.sort(new BookAuthorPagesComparator());
+
+    // assert - сравнить результат с правильным
+    assertEquals(expected, actual);
   }
 }
