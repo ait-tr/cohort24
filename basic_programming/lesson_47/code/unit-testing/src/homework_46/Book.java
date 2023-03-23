d2db7e2fe4e84286ba9b5b492e142ddf2561dbc0
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-23 17:41:01 +0100

    Implement Comparable<Book>

--- basic_programming/lesson_47/code/unit-testing/src/homework_46/Book.java
+++ basic_programming/lesson_47/code/unit-testing/src/homework_46/Book.java
@@ -1,6 +1,6 @@
 package homework_46;
 
-public class Book {
+public class Book implements Comparable<Book> {
 
   private final String title;
   private final String author;
@@ -22,5 +22,14 @@ public class Book {
 
   public int getPages() {
     return pages;
+  }
+
+  // "натуральная сортировка" - автор, потом название
+  @Override
+  public int compareTo(Book o) {
+    if (!author.equals(o.author)) {
+      return author.compareTo(o.author);
+    }
+    return title.compareTo(o.title);
   }
 }
