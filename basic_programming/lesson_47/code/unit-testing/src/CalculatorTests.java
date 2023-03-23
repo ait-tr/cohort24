e772a8af4073c133f3152e3a5e298f4acd166020
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-23 20:31:55 +0100

    Add some CalculatorTests

--- basic_programming/lesson_47/code/unit-testing/src/CalculatorTests.java
+++ basic_programming/lesson_47/code/unit-testing/src/CalculatorTests.java
@@ -33,4 +33,21 @@ public class CalculatorTests {
     assertEquals(-5, calc.add(-5, 0));
     assertEquals(-5, calc.add(0, -5));
   }
+
+  @Test
+  public void addCommutative() {
+    // можно сравнивать не ожидание и реальность, а две реальности
+    assertEquals(calc.add(3, 57), calc.add(57, 3));
+  }
+
+  @Test
+  public void addPositiveZero() {
+    assertEquals(5, calc.add(5, 0));
+    assertEquals(5, calc.add(0, 5));
+  }
+
+  @Test
+  public void addZeros() {
+    assertEquals(0, calc.add(0, 0));
+  }
 }
