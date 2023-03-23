546640895207a8e3ce211764e1023da0e42658f0
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-23 17:36:34 +0100

    Add Book

---
+++ basic_programming/lesson_47/code/unit-testing/src/homework_46/Book.java
@@ -0,0 +1,14 @@
+package homework_46;
+
+public class Book {
+
+  private String title;
+  private String author;
+  private int pages;
+
+  public Book(String title, String author, int pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+  }
+}
