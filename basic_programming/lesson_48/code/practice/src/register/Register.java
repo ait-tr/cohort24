dc9952569b98bc6bcb234568d2a6e213952f3c56
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-24 20:19:49 +0100

    Add register logic

--- basic_programming/lesson_48/code/practice/src/register/Register.java
+++ basic_programming/lesson_48/code/practice/src/register/Register.java
@@ -6,16 +6,21 @@ import java.util.List;
 public class Register {
 
   private final List<Receipt> receipts = new ArrayList<>();
+  private Receipt current = new Receipt(); // текущий (открытый) чек
 
   public void addLine() {
     System.out.println("Добавляем товар в чек");
   }
 
   public void newReceipt() {
+    // TODO проверка, что "старый" чек не пустой
+    receipts.add(current); // добавляем "старый" текущий чек в список закрытых
+    current = new Receipt(); // открываем ещё один (новый) чек
     System.out.println("Начинаем новый чек");
   }
 
   public void printReport() {
+    // TODO закрыть последний открытый чек - обычно мы добавляем чеки только после закрытия
     System.out.println("Выводим отчёт");
   }
 }
