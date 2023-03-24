93c34a7ea85420e24f054820c468b4646f9c90b3
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-24 20:38:03 +0100

    Add ReceiptLine

--- basic_programming/lesson_48/code/practice/src/register/ReceiptLine.java
+++ basic_programming/lesson_48/code/practice/src/register/ReceiptLine.java
@@ -2,4 +2,28 @@ package register;
 
 public class ReceiptLine {
 
+  private final String title;
+  private final int price;
+
+  public ReceiptLine(String title, int price) {
+    if (title == null) {
+      throw new IllegalArgumentException("Название позиции в чеке не может быть null");
+    }
+    if (title.isEmpty()) {
+      throw new IllegalArgumentException("Название позиции в чеке не может быть пустым");
+    }
+    this.title = title;
+    if (price <= 0) {
+      throw new IllegalArgumentException("Цена должна быть положительной");
+    }
+    this.price = price;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public int getPrice() {
+    return price;
+  }
 }
