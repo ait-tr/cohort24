d41b792728a2dac3754815c25657e9c69e939306
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-24 17:50:50 +0100

    Add BookTitleAuthorComparatorTests.differentTitleComparison()

--- basic_programming/lesson_48/code/practice/src/homework_47/BookTitleAuthorComparatorTests.java
+++ basic_programming/lesson_48/code/practice/src/homework_47/BookTitleAuthorComparatorTests.java
@@ -1,9 +1,29 @@
 package homework_47;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Comparator;
+import org.junit.jupiter.api.Test;
+
 // Напишите автотесты с использованием JUnit для BookTitleAuthorComparator.
 //
 // Проверьте краевые случаи, предусмотрите разные варианты.
 public class BookTitleAuthorComparatorTests {
 
+  private Comparator<Book> comparator = new BookTitleAuthorComparator();
 
+  @Test
+  public void differentTitleComparison() {
+    // сравниваем две книги с разными названиями
+
+    // arrange
+    Book book1 = new Book("Author", "A", 25);
+    Book book2 = new Book("Author", "B", 25);
+
+    // act
+    int result = comparator.compare(book1, book2);
+
+    // assert
+    assertEquals(true, result < 0);
+  }
 }
