aea4c4073b66454623a555178da798bd6e364c10
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-24 17:44:36 +0100

    Add homework_47 template

---
+++ basic_programming/lesson_48/code/practice/src/homework_47/BookTitleAuthorComparatorTests.java
@@ -0,0 +1,9 @@
+package homework_47;
+
+// Напишите автотесты с использованием JUnit для BookTitleAuthorComparator.
+//
+// Проверьте краевые случаи, предусмотрите разные варианты.
+public class BookTitleAuthorComparatorTests {
+
+
+}
