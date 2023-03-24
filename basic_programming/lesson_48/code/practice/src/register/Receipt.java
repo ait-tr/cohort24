1d77c71a500ea32d9430245a373cf52d42f94ba2
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-24 21:03:53 +0100

    Add Receipt.total

--- basic_programming/lesson_48/code/practice/src/register/Receipt.java
+++ basic_programming/lesson_48/code/practice/src/register/Receipt.java
@@ -6,12 +6,14 @@ import java.util.List;
 public class Receipt {
 
   private final List<ReceiptLine> lines = new ArrayList<>();
+  private int total = 0;
 
   public void addLine(ReceiptLine line) {
     if (line == null) {
       throw new IllegalArgumentException("Строка чека не может быть null");
     }
     lines.add(line);
+    total += line.getPrice();
   }
 
   public boolean isEmpty() {
@@ -24,6 +26,12 @@ public class Receipt {
     for (ReceiptLine line : lines) {
       result += line + "\n";
     }
+    result += "----------------\n";
+    result += "Общий итог: " + total;
     return result;
+  }
+
+  public int getTotal() {
+    return total;
   }
 }
