c0ee5c1a459c48ae96afd2cb3e14764a88b2d6a5
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-27 20:07:31 +0200

    Add Date

--- basic_programming/lesson_49/code/date/src/Main.java
+++ basic_programming/lesson_49/code/date/src/Main.java
@@ -1,10 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Date;
 
 public class Main {
-
-  private static final int CURRENT_YEAR = 2023;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,7 +17,14 @@ public class Main {
 //      System.out.println("Извините, дата рождения не может быть в будущем!");
 //    }
 
-    int age = CURRENT_YEAR - year;
+    Date current = new Date(); // текущая дата и время
+    // Date - количество миллисекунд с момента 00:00:00 UTC 01.01.1970
+    // например, 1679939243000 миллисекунд - это 2023-03-27 17:47:23.000
+
+    // getYear() вернёт год. Но:
+    // - он deprecated (устаревший) и больше не поддерживается
+    // - он возвращает количество лет, прошедшее с 1900 года
+    int age = 1900 + current.getYear() - year;
     System.out.println("Похоже, тебе " + age + " лет. Или " + (age - 1) + "!");
   }
 }
