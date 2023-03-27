992dfb3e6ea99bd01a68e7664c90c67c3903bbc1
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-27 19:48:15 +0200

    Add age calculation

---
+++ basic_programming/lesson_49/code/date/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Привет! Как тебя зовут?");
+    String name = br.readLine();
+
+    // женский род, потому что слово "программа" женского рода
+    System.out.println("Рада познакомиться, " + name + "! А какого ты года рождения?");
+    int year = Integer.parseInt(br.readLine());
+
+    int age = 2023 - year;
+    System.out.println("Похоже, тебе " + age + " лет.");
+  }
+}
