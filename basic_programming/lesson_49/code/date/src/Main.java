e493221d0a15d3a5257fa04835c3a07087185a7d
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-27 19:52:07 +0200

    Add CURRENT_YEAR

--- basic_programming/lesson_49/code/date/src/Main.java
+++ basic_programming/lesson_49/code/date/src/Main.java
@@ -4,6 +4,8 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+  private static final int CURRENT_YEAR = 2023;
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     System.out.println("Привет! Как тебя зовут?");
@@ -12,8 +14,11 @@ public class Main {
     // женский род, потому что слово "программа" женского рода
     System.out.println("Рада познакомиться, " + name + "! А какого ты года рождения?");
     int year = Integer.parseInt(br.readLine());
+//    if (year > CURRENT_YEAR) {
+//      System.out.println("Извините, дата рождения не может быть в будущем!");
+//    }
 
-    int age = 2023 - year;
+    int age = CURRENT_YEAR - year;
     System.out.println("Похоже, тебе " + age + " лет. Или " + (age - 1) + "!");
   }
 }
