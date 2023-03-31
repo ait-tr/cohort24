fe41854b48010a41b276da0c11cd97635237401b
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-03-31 21:06:58 +0200

    Add fieldToString()

--- basic_programming/lesson_53/code/practice/src/TicTacToe.java
+++ basic_programming/lesson_53/code/practice/src/TicTacToe.java
@@ -4,6 +4,13 @@ import java.io.IOException;
 public class TicTacToe { // Крестики-нолики
 
   private static final int SIZE = 3; // размер игрового поля
+  // char[] row = new char[SIZE] // ['a', 'b', 'c']
+  // char[][] field = new char[SIZE][SIZE] // [row1, row2, row3]
+  // [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i']]
+  // [['a', 'b', 'c'],
+  //  ['d', 'e', 'f'],
+  //  ['g', 'h', 'i']]
+  char[][] field = new char[SIZE][SIZE];
 
   // Запуск и в конце получение результата
   // игра начинается:
@@ -22,6 +29,7 @@ public class TicTacToe { // Крестики-нолики
       Point newPoint = readCoordinates(bufferedReader);
       // Point - координаты символа, readCoordinates - метод для их получения
       makeStep(newPoint); // makeStep - метод для того, чтобы сделать ход
+      System.out.println(fieldToString());
       finished = checkField(); // checkField - метод для проверки того, завершена ли игра
     }
     return getResult(); // getResult() - метод для получения строки с результатом
@@ -38,7 +46,8 @@ public class TicTacToe { // Крестики-нолики
   }
 
   private void makeStep(Point newPoint) {
-    // TODO
+    // координаты 1..3 превращаем в индексы 0..2
+    field[newPoint.getX() - 1][newPoint.getY() - 1] = 'X';
   }
 
   private Point readCoordinates(BufferedReader bufferedReader) throws IOException {
@@ -56,5 +65,39 @@ public class TicTacToe { // Крестики-нолики
     if (y < 1 || y > SIZE) {
       throw new IllegalArgumentException("Некорректная координата y: " + y);
     }
+  }
+
+  public String fieldToString() {
+    String result = "";
+
+    char CORNER = '+';
+    char HORIZONTAL = '-';
+    char VERTICAL = '|';
+
+    // горизонтальный разделитель
+    result += CORNER;
+    for (int i = 0; i < SIZE; ++i) {
+      result += HORIZONTAL;
+      result += CORNER;
+    }
+    result += "\n";
+
+    for (int rowI = 0; rowI < SIZE; ++rowI) {
+      result += VERTICAL;
+      for (int colI = 0; colI < SIZE; ++colI) {
+        result += field[rowI][colI];
+        result += VERTICAL;
+      }
+      result += "\n";
+      // горизонтальный разделитель
+      result += CORNER;
+      for (int i = 0; i < SIZE; ++i) {
+        result += HORIZONTAL;
+        result += CORNER;
+      }
+      result += "\n";
+    }
+
+    return result;
   }
 }
