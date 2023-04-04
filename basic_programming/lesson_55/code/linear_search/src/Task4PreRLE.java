7b705c5cec163d03a0a665e57aec1ef95c4e638a
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-04 20:35:19 +0200

    Add Task4PreRLE solution

--- basic_programming/lesson_55/code/linear_search/src/Task4PreRLE.java
+++ basic_programming/lesson_55/code/linear_search/src/Task4PreRLE.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 public class Task4PreRLE {
 
   // Run-length encoding (RLE) (aaabbcddddaaaaa = a3b2cd4a5)
-  // Промежуточная задача - PreRLE - убрать повторяющиеся подряд буквы
+  // Промежуточная задача - PreRLE - убрать повторяющиеся подряд символы
   // (aaabbcddddaaaaa = abcda)
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,7 +13,14 @@ public class Task4PreRLE {
     String input = br.readLine();
     String result = "";
 
-    // TODO
+    Character prev = null; // предыдущий символ, в начале он null - его нет
+    for (int i = 0; i < input.length(); ++i) {
+      Character c = input.charAt(i); // текущий символ
+      if (/*prev == null || */!c.equals(prev)) { // если текущий не совпадает с предыдущим
+        result += c; // записываем текущий (не совпадающий) в результат
+        prev = c;
+      }
+    }
 
     System.out.println("Промежуточный результат сжатия:");
     System.out.println(result);
