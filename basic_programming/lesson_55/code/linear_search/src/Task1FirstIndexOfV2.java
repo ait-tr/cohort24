b8625428dee48d4c449e4064f563332a8d82f694
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-04 19:57:24 +0200

    Update Task1FirstIndexOfV2 solution

--- basic_programming/lesson_55/code/linear_search/src/Task1FirstIndexOfV2.java
+++ basic_programming/lesson_55/code/linear_search/src/Task1FirstIndexOfV2.java
@@ -8,33 +8,25 @@ public class Task1FirstIndexOfV2 {
 
   // Найти первое (левое) вхождение положительного числа X.
   // Вывести индекс или -1, если число не встречалось.
-  public static void main(String[] args) throws IOException { // O(n) - time
+  public static void main(String[] args) throws IOException { // O(n) - time, O(1) - space
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    List<Integer> numbers = new ArrayList<>(); // O(1)
-
-    System.out.print("Введите количество чисел: "); // O(1)
-    int n = Integer.parseInt(br.readLine()); // O(1)
-    System.out.println("Введите " + n + " чисел, каждое с новой строки:"); // O(1)
-    for (int i = 0; i < n; ++i) { // O((1 + 1) * n) = O(2 * n) = O(n)
-      int x = Integer.parseInt(br.readLine()); // O(1)
-      numbers.add(x); // O(1)
-    }
 
     System.out.print("Введите число для поиска: ");
     int target = Integer.parseInt(br.readLine());
-
     int result = -1; // ответ "по умолчанию", если мы его не изменим
-    for (int i = 0; i < numbers.size(); ++i) { // O(n)
-      if (numbers.get(i) == target) { // O(1)
+
+    System.out.print("Введите количество чисел: ");
+    int n = Integer.parseInt(br.readLine());
+
+    System.out.println("Введите " + n + " чисел, каждое с новой строки:");
+    for (int i = 0; i < n; ++i) { // O(n) - time, O(1) - space
+      int x = Integer.parseInt(br.readLine()); // O(1) - time, O(1) - space
+      if (result == -1 && x == target) { // если результат не менялся и число - нужное нам
         // i - наш ответ
         result = i; // нашли и изменили ответ
       }
     }
-    // если мы дошли до этого места, то мы не нашли число target
-    if (result != -1) {
-      System.out.println("numbers[" + result + "] = " + numbers.get(result)); // O(1)
-    } else {
-      System.out.println("Число не найдено");
-    }
+
+    System.out.println("Результат: " + result);
   }
 }
