ecbc6da41cb3a902149b1d098c968b86f9cfac4a
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-05 19:49:12 +0200

    Add Task2IndexOf

---
+++ basic_programming/lesson_56/code/binary_search/src/Task2IndexOf.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task2IndexOf {
+
+  /**
+   * Поиск числа target в списке numbers
+   * @param numbers отсортированный по возрастанию список различных целых чисел
+   * @param target число, которое необходимо найти
+   * @return индекс числа target в списке numbers или -1, если число не найдено
+   */
+  public static int indexOf(List<Integer> numbers, int target) {
+    // индексы left и right сделаем "как везде" - left включая, right - не включая
+    int left = 0;
+    int right = numbers.size();
+    // пока размер области поиска больше одного элемента
+    while (left < right - 1) { // right - left > 1
+      int mid = (left + right) / 2;
+      if (numbers.get(mid) == target) {
+        return mid;
+      }
+      if (numbers.get(mid) < target) {
+        left = mid + 1;
+      } else { // не = и не <
+        right = mid; // потому что right - не включая
+      }
+    }
+    // после окончания цикла left + 1 = right, и проверяем мы numbers[left]
+    if (numbers.get(left) == target) {
+      return left;
+    }
+    return -1; // сужение области поиска не дало нам target - его нет в списке
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.print("Введите количество элементов: ");
+    int n = Integer.parseInt(br.readLine());
+
+    List<Integer> numbers = new ArrayList<>(n);
+    for (int i = 0; i < n; ++i) {
+      numbers.add(Integer.parseInt(br.readLine()));
+    }
+
+    System.out.print("Введите число для поиска: ");
+    int target = Integer.parseInt(br.readLine());
+
+    System.out.println("Результат: " + indexOf(numbers, target));
+  }
+}
