f474aefc0adc7d1715bea30752ca9a85960cc956
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-06 18:44:28 +0200

    Add foundPositiveParametrized

--- basic_programming/lesson_57/code/practice/src/Task1MaxOddTests.java
+++ basic_programming/lesson_57/code/practice/src/Task1MaxOddTests.java
@@ -6,6 +6,7 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class Task1MaxOddTests {
@@ -51,5 +52,22 @@ public class Task1MaxOddTests {
         Arguments.of(new ArrayList<>(Arrays.asList(2, 4, 6)), -1),
         Arguments.of(new ArrayList<>(Arrays.asList(-2, -4, -6)), -1)
     );
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+      "1, 2, 3, 4, 2",
+  })
+  public void foundPositiveParametrized(int a, int b, int c, int d, int expected) {
+//    ArrayList<Integer> numbers = new ArrayList<>();
+//    numbers.add(a);
+//    numbers.add(b);
+//    numbers.add(c);
+//    numbers.add(d);
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(a, b, c, d));
+
+    int index = Task1MaxOdd.firstIndexOfMaxOdd(numbers);
+
+    assertEquals(expected, index);
   }
 }
