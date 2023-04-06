b31d831803ae56c07395cf8d3c11cf251f49f1ee
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-06 17:48:53 +0200

    Correct Homework56IndexOfDescending solution

--- basic_programming/lesson_57/code/practice/src/Homework56IndexOfDescending.java
+++ basic_programming/lesson_57/code/practice/src/Homework56IndexOfDescending.java
@@ -16,7 +16,7 @@ public class Homework56IndexOfDescending {
    * @return индекс числа в списке или -1, если число не найдено
    */
   public static int indexOf(ArrayList<Integer> descendingNumbers, int target) {
-    int left = 0;
+    int left = 0; // границы области поиска (индексы)
     int right = descendingNumbers.size(); // O(1)
     // пока размер области поиска больше одного элемента
     while (left < right - 1) { // right - left > 1 // O(log N)
@@ -25,9 +25,13 @@ public class Homework56IndexOfDescending {
         return mid;
       }
       if (descendingNumbers.get(mid) < target) { // ArrayList.get() - O(1)
-        left = mid + 1;
-      } else { // не = и не <
-        right = mid; // потому что right - не включая
+        // список отсортирован по убыванию
+        // если середина меньше, чем наша цель, то наша цель где-то СЛЕВА (раньше)
+        right = mid; // сдвигаем правую границу - её не включаем, поэтому просто mid
+      } else { // не = и не <, значит, >
+        // список отсортирован по убыванию
+        // если середина больше, чем наша цель, то наша цель где-то СПРАВА (позже)
+        left = mid + 1; // сдвигаем левую границу - её включаем, поэтому mid + 1
       }
     }
     // после окончания цикла right - left = 0 или 1, и проверяем мы numbers[left]
