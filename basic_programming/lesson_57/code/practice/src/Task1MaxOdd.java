c385a55bf7c60c7b197882e4c687bb9501db63da
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-06 18:27:04 +0200

    Add an algorithm for LinkedList

--- basic_programming/lesson_57/code/practice/src/Task1MaxOdd.java
+++ basic_programming/lesson_57/code/practice/src/Task1MaxOdd.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Task1MaxOdd {
 
@@ -23,6 +25,27 @@ public class Task1MaxOdd {
         maxOdd = x;
         result = i;
       }
+    }
+    return result;
+  }
+
+  /**
+   * Поиск первого появления максимального нечётного числа
+   * @param numbers список чисел
+   * @return индекс максимального нечётного числа или -1, если нечётных чисел нет
+   */
+  public static int firstIndexOfMaxOdd(LinkedList<Integer> numbers) {
+    int maxOdd = DUMMY; // максимальный нечётный элемент
+    int result = -1; // индекс максимального нечётного элемента
+
+    int i = 0; // хак для LinkedList: get(i) работает как O(n), а вот for-each переходит за O(1)
+    for (int x : numbers) { // O(n)
+      // если элемент чётный И (результат не менялся ИЛИ элемент больше рамочки)
+      if (x % 2 != 0 && (result == -1 || x > maxOdd)) {
+        maxOdd = x;
+        result = i;
+      }
+      ++i;
     }
     return result;
   }
