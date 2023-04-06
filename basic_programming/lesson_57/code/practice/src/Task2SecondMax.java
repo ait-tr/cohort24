cadbde3bcb4a0e6438af9218dc96b0f9fe050697
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-06 19:32:08 +0200

    Remove Task2SecondMax booleans

--- basic_programming/lesson_57/code/practice/src/Task2SecondMax.java
+++ basic_programming/lesson_57/code/practice/src/Task2SecondMax.java
@@ -10,18 +10,15 @@ public class Task2SecondMax {
    * @return индекс второго по величине числа или -1, если такого нет
    */
   public static int firstIndexOfSecondMax(List<Integer> numbers) {
-    boolean firstFound = false;
     int firstMax = Integer.MIN_VALUE; // первый максимум
     int firstMaxIndex = -1;
-    boolean secondFound = false;
     int secondMax = Integer.MIN_VALUE; // второй максимум
     int secondMaxIndex = -1;
 
     int i = 0;
     for (int x : numbers) {
-      if (!firstFound) { // если даже в первом максимуме ничего не было
+      if (firstMaxIndex == -1) { // если даже в первом максимуме ничего не было
         firstMax = x;
-        firstFound = true;
         firstMaxIndex = i;
       } else if (x > firstMax) { // здесь и дальше первый максимум есть
         // было:         ..., secondMaximum, firstMaximum
@@ -29,12 +26,10 @@ public class Task2SecondMax {
         // должно стать:      ...,           secondMaximum, firstMaximum
         secondMax = firstMax;
         secondMaxIndex = firstMaxIndex; // ради этой строчки нужно помнить firstMaxIndex
-        secondFound = true; // перый максимум уже был, значит, и secondMax теперь точно есть
         firstMax = x;
         firstMaxIndex = i;
-      } else if (!secondFound) { // здесь и дальше x < firstMaximum
+      } else if (secondMaxIndex == -1) { // здесь и дальше x < firstMaximum
         secondMax = x;
-        secondFound = true;
         secondMaxIndex = i;
       } else if (x > secondMax) { // второй максимум тоже был
         // было:         ..., secondMaximum,                firstMaximum
