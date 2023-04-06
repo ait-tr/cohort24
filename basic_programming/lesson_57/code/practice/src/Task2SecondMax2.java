1fef7077e4a170081014efc1263e6b60584040c3
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-06 19:50:02 +0200

    Add Task2SecondMax2 solution

--- basic_programming/lesson_57/code/practice/src/Task2SecondMax2.java
+++ basic_programming/lesson_57/code/practice/src/Task2SecondMax2.java
@@ -8,14 +8,24 @@ public class Task2SecondMax2 {
    * @param numbers список чисел
    * @return индекс второго по величине числа или -1, если такого нет
    */
-  public static int firstIndexOfSecondMax(List<Integer> numbers) {
+  public static int firstIndexOfSecondMax(List<Integer> numbers) { // O(n) время, O(1) доп. память
     int firstMax = Integer.MIN_VALUE; // первый максимум
     int secondMax = Integer.MIN_VALUE; // второй максимум
     int secondMaxIndex = -1;
 
-    int i = 0;
     for (int x : numbers) {
-      // TODO
+      if (x > firstMax) {
+        firstMax = x;
+      }
+    }
+
+    int i = 0;
+    for (int x : numbers) { // из-за второго перебора эту задачу не свести к O(1) по памяти
+      if (x < firstMax && (secondMaxIndex == -1 || x > secondMax)) {
+        // [1, Integer.MIN_VALUE] - не то же самое, что [1, 1]
+        secondMax = x;
+        secondMaxIndex = i;
+      }
       ++i;
     }
     return secondMaxIndex;
