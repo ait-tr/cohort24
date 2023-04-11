3a2285ae04060b15a3ba7b4ef78456521a611dd3
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-11 20:54:51 +0200

    Fix Task3BinarySearch recursive solution

--- basic_programming/lesson_58/code/recursion/src/Task3BinarySearch.java
+++ basic_programming/lesson_58/code/recursion/src/Task3BinarySearch.java
@@ -5,31 +5,34 @@ import java.io.InputStreamReader;
 public class Task3BinarySearch {
 
   public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     System.out.println("Загадайте число от 1 до 1000");
     System.out.println("На все вопросы отвечайте символами '>', '<' или '='");
-    int number = guess(1, 1000);
+    int number = guess(bufferedReader, 1, 1000);
     System.out.println("Вы загадали число " + number + "!");
   }
 
-  public static int guess(int left, int right) throws IOException {
-    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+  public static int guess(BufferedReader bufferedReader, int left, int right) throws IOException {
+    if (left >= right) { // > никогда не должно быть, но я подстраховываюсь
+      return left; // настоящий выход из рекурсии
+    }
 
     int mid = (left + right) / 2;
     System.out.println("Ваше число - " + mid + "?");
     String answer = bufferedReader.readLine();
     switch (answer) {
       case ">" -> {
-        return guess(mid + 1, right);
+        return guess(bufferedReader, mid + 1, right);
       }
       case "<" -> {
-        return guess(left, mid - 1);
+        return guess(bufferedReader, left, mid - 1);
       }
       case "=" -> {
         return mid; // досрочный выход из рекурсии
       }
       default -> {
         System.out.println("На все вопросы отвечайте символами '>', '<' или '='");
-        return guess(left, right); // повторяем вызов - ответ непонятен
+        return guess(bufferedReader, left, right); // повторяем вызов - ответ непонятен
       }
     }
   }
