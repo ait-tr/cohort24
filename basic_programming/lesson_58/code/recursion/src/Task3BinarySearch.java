34ab0fe0a372e6ab865407ee64aad6f5407080cf
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-11 20:37:52 +0200

    Add Task3BinarySearch template

---
+++ basic_programming/lesson_58/code/recursion/src/Task3BinarySearch.java
@@ -0,0 +1,13 @@
+public class Task3BinarySearch {
+
+  public static void main(String[] args) {
+    System.out.println("Загадайте число от 1 до 1000");
+    System.out.println("На все вопросы отвечайте символами '>', '<' или '='");
+    int number = guess(1, 1000);
+    System.out.println("Вы загадали число " + number);
+  }
+
+  public static int guess(int left, int right) {
+    return 0;
+  }
+}
