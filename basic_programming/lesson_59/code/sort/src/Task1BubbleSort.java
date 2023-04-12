82889e572a8080178e7a6d8c166df8b37ff539d4
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-12 18:24:13 +0200

    Fix pairs indices

--- basic_programming/lesson_59/code/sort/src/Task1BubbleSort.java
+++ basic_programming/lesson_59/code/sort/src/Task1BubbleSort.java
@@ -3,14 +3,23 @@ import java.util.ArrayList;
 public class Task1BubbleSort {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    ArrayList<Integer> numbers = new ArrayList<>();
+    numbers.add(3);
+    numbers.add(1);
+    numbers.add(7);
+    numbers.add(2);
+    numbers.add(5);
+
+    System.out.println(numbers);
+    sort(numbers);
+    System.out.println(numbers);
   }
 
   public static void sort(ArrayList<Integer> numbers) {
     for (int right = numbers.size(); right > 0; --right) {
       // right - правая граница области поиска (не включая)
       // перебираем все индексы от 0 (включая) до right (не включая)
-      for (int i = 0; i < right; ++i) {
+      for (int i = 0; i < right - 1; ++i) { // "левыми" индексами могут быть все, кроме последнего
         int first = numbers.get(i);
         int second = numbers.get(i + 1);
         if (first > second) {
