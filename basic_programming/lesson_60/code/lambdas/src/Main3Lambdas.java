612c72cc9aab72647bad2ed624c2bcf5b4762b9a
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-13 20:32:13 +0200

    Move StudentScoreComparator to Main3Lambdas

--- basic_programming/lesson_60/code/lambdas/src/Main3Lambdas.java
+++ basic_programming/lesson_60/code/lambdas/src/Main3Lambdas.java
@@ -4,6 +4,16 @@ import java.util.List;
 
 public class Main3Lambdas {
 
+  // nested class - inner class - класс, определённый в теле другого
+  // private class не будет доступен даже соседним файлам в этой папке
+  private static class StudentScoreComparator implements Comparator<Student> {
+
+    @Override
+    public int compare(Student o1, Student o2) {
+      return o2.getScore() - o1.getScore(); // по убыванию среднего балла
+    }
+  }
+
   public static void main(String[] args) {
     List<Student> students = new ArrayList<>();
     students.add(new Student("Anton", 3));
