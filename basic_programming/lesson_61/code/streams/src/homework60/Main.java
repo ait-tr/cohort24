bd4cca199abca0ba38d3b823623aa0270b355511
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-14 17:43:12 +0200

    Update homework_60 template

--- basic_programming/lesson_61/code/streams/src/homework60/Main.java
+++ basic_programming/lesson_61/code/streams/src/homework60/Main.java
@@ -1,10 +1,25 @@
 package homework60;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
   // Используйте класс `Book` (книга) из домашнего задания к уроку 45.
   //
   // Создайте список книг и отсортируйте книги по названию,
   // (при одинаковых названиях - по автору) при помощи лямбда-функции.
+  public static void main(String[] args) {
+    List<Book> books = new ArrayList<>();
 
+    books.add(new Book("Author 3", "Book 3", 250));
+    books.add(new Book("Author 1", "Book 2", 251));
+    books.add(new Book("Author 3", "Book 1", 252));
+
+//    books.sort();
+    for (Book b : books) {
+      System.out.println(b);
+    }
+  }
 }
