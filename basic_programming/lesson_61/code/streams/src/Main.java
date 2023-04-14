5c332990d5fc6cd7aa981aae2b374ff85a14d5f7
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-14 17:59:21 +0200

    Add number squares print

--- basic_programming/lesson_61/code/streams/src/Main.java
+++ basic_programming/lesson_61/code/streams/src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int[] numbers = /*new int[]*/{1, 2, 3, 4, 5};
+    System.out.println("Список чисел:");
+    for (int x : numbers) {
+      System.out.println(x);
+    }
+
+    for (int i = 0; i < numbers.length; ++i) {
+      numbers[i] *= numbers[i];
+    }
+    System.out.println("Список квадратов чисел:");
+    for (int x : numbers) {
+      System.out.println(x);
+    }
   }
 }
