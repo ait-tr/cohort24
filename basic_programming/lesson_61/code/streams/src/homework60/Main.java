57efd1bc8b0d8c63aa2da5d76a271ed99fab53a2
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-14 17:48:58 +0200

    Add homework_60 solution

--- basic_programming/lesson_61/code/streams/src/homework60/Main.java
+++ basic_programming/lesson_61/code/streams/src/homework60/Main.java
@@ -1,7 +1,6 @@
 package homework60;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -13,11 +12,16 @@ public class Main {
   public static void main(String[] args) {
     List<Book> books = new ArrayList<>();
 
-    books.add(new Book("Author 3", "Book 3", 250));
+    books.add(new Book("Author 2", "Book 3", 250));
     books.add(new Book("Author 1", "Book 2", 251));
     books.add(new Book("Author 3", "Book 1", 252));
 
-//    books.sort();
+    books.sort((o1, o2) -> {
+      if (!o1.getTitle().equals(o2.getTitle())) {
+        return o1.getTitle().compareTo(o2.getTitle());
+      }
+      return o1.getAuthor().compareTo(o2.getAuthor());
+    });
     for (Book b : books) {
       System.out.println(b);
     }
