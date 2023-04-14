57bcd5d56e3bda9494e422f692b0360b9d1fc09d
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-14 18:16:20 +0200

    Add stream example

--- basic_programming/lesson_61/code/streams/src/Main.java
+++ basic_programming/lesson_61/code/streams/src/Main.java
@@ -1,18 +1,35 @@
+import java.util.stream.Stream;
+
 public class Main {
 
   public static void main(String[] args) {
+    // создание
     int[] numbers = /*new int[]*/{1, 2, 3, 4, 5};
     System.out.println("Список чисел:");
     for (int x : numbers) {
       System.out.println(x);
     }
 
+    // изменение
     for (int i = 0; i < numbers.length; ++i) {
       numbers[i] *= numbers[i];
     }
+
     System.out.println("Список квадратов чисел:");
+    // потребление
     for (int x : numbers) {
       System.out.println(x);
     }
+
+    System.out.println("Список квадратов чисел из потока:");
+    Stream.of(1, 2, 3, 4, 5)    // создание
+        .map(x -> x * x)                // изменение
+        .forEach(Main::strangePrint);   // потребление
+    // класс::метод -- ссылка на метод (method reference)
+    // именно так можно передавать метод аналогично лямбда-функции - теперь это НЕ анонимная функция
+  }
+
+  public static void strangePrint(int x) {
+    System.out.println("-=" + x + "=-");
   }
 }
