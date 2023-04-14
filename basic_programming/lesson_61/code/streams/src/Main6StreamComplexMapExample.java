3748caa6f39cf55005910dd8dd4e522605d3fda3
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-04-14 20:00:37 +0200

    Add Main6StreamComplexMapExample comments

--- basic_programming/lesson_61/code/streams/src/Main6StreamComplexMapExample.java
+++ basic_programming/lesson_61/code/streams/src/Main6StreamComplexMapExample.java
@@ -6,9 +6,9 @@ public class Main6StreamComplexMapExample {
   public static void main(String[] args) {
     // создание
     List<List<String>> titles = List.of(
-        List.of("Неизвестен", "Букварь"),
-        List.of("Некто", "Вторая"),
-        List.of("Известен", "Синяя")
+        List.of("Неизвестен", "Букварь"), // List<String> info
+        List.of("Некто", "Вторая"),       // List<String> info
+        List.of("Известен", "Синяя")      // List<String> info
     );
     titles.stream() // здесь поток строк (названий)
         .map(info -> new Book(info.get(0), info.get(1), 123)) // а здесь уже поток книг
