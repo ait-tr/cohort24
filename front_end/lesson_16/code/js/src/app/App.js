d1ccb4e0c1afd61f9e4527ce1298ce1fc7268d19
surovceva <surovceva@protei-lab.ru>
2023-05-27 01:03:12 +0300

    Cohort24_lesson_16 (theory+homework+code+metadata)

---
+++ front_end/lesson_16/code/js/src/app/App.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  // let count = 0
+  // count - переменная
+  // setCount - функция, которая меняет переменную count
+  const [count, setCount] = useState(0)
+  const [step, setStep] = useState(1)
+
+  function handleClickMinus() {
+    setCount(count - step) 
+  }
+  function handleClickPlus() {
+    setCount(count + step) 
+  }
+
+  function handleChange(event) {
+   const value = event.target.value
+    setStep(Number(value))
+  }
+  // console.log(step)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello</p>
+        <div>
+        <button onClick={handleClickMinus}>-</button> {"  "}
+        <span>{count}</span> {"  "}
+        <button onClick={handleClickPlus}>+</button>
+        </div>
+        <input 
+        value={step} 
+        onChange={handleChange} 
+        onClick={handleClickPlus}
+        type='number'/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
