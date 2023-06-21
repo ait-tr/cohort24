bea7eb2d3961a687ce0da37cd0dcb1bd18c7371e
Ira Baker <49585685+irinabaker@users.noreply.github.com>
2023-06-21 21:23:52 +0200

    Lesson_9

---
+++ qa/lesson_9/code/PhoneBook/src/test/java/com/phonebook/tests/ApplicationManager.java
@@ -0,0 +1,56 @@
+package com.phonebook.tests;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class ApplicationManager {
+
+    String browser;
+    WebDriver driver;
+
+    UserHelper user;
+    ContactHelper contact;
+    HomePageHelper homePage;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
+    public UserHelper getUser() {
+        return user;
+    }
+
+    public ContactHelper getContact() {
+        return contact;
+    }
+
+    public HomePageHelper getHomePage() {
+        return homePage;
+    }
+
+    public void init() {
+        System.err.close();
+
+        if (browser.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+        }
+
+        driver.get("https://telranedu.web.app");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        user = new UserHelper(driver);
+        contact = new ContactHelper(driver);
+        homePage = new HomePageHelper(driver);
+    }
+
+    public void stop() {
+        driver.quit();
+    }
+
+}
---
+++ qa/lesson_9/plan.md
@@ -0,0 +1,6 @@
+## Задачи на урок:
+
+1. Selenium. Value Object Model
+2. @BeforeSuite
+3. Run tests from different browsers
+4. testng.xml
