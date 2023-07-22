cc9cf389016cdee30c5c9dcadbc7026bfac24a8c
Sergey Iryupin <biblelamp@gmail.com>
2023-07-22 04:17:16 +0200

    lesson #8 code update

--- back_end/lesson_08/code/src/main/java/spring/controller/dto/EventDTO.java
+++ back_end/lesson_08/code/src/main/java/spring/controller/dto/EventDTO.java
@@ -1,6 +1,10 @@
 package spring.controller.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import spring.domain.Event;
 
 @Getter
@@ -12,6 +16,7 @@ public class EventDTO {
     private Integer id;
     private String name;
     private String city;
+
     public static EventDTO getInstance(Event event) {
         return new EventDTO(event.getEventId(), event.getName(), event.getCity().getName());
     }
